1ba16bd1cc64a97faa571038c6ee609858de33f0
Diecodev <diegoddiaz05@gmail.com>
2026-06-22 23:24:32 -0500

    Initial Commit

---
+++ Echo/EchoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EchoApp.swift
+//  Echo
+//
+//  Created by Diego Díaz on 22/06/26.
+//
+
+import SwiftUI
+
+@main
+struct EchoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
